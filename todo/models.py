1e6eeb49607ae9641e673fa8a421e3a98ae95876
Masaya Nakayama <uw.msy.0913@gmail.com>
2020-04-21 11:59:16 +0900

    userテーブルの削除

--- todo/models.py
+++ todo/models.py
@@ -1,24 +1,9 @@
 from django.db import models
 
-class User(models.Model):
-    username = models.CharField("ユーザー名", max_length=50)
-    email = models.EmailField("メールアドレス", max_length=255)
-    password = models.CharField("パスワード", max_length=50)
-
-    def __str__(self):
-        return self.username
-
-    class Meta:
-        db_table = "user"
-        verbose_name = "ユーザー"
-        verbose_name_plural = "ユーザー"
-
-
 class Todo(models.Model):
     title = models.CharField("トレーニング名", max_length=50)
     times = models.PositiveIntegerField("回数")
     complete_at = models.DateTimeField('完了日時', null=True)
-    user = models.ForeignKey("User", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
