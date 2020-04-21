63fd912d3611ffa99153634ca7d77a1663499d79
Masaya Nakayama <uw.msy.0913@gmail.com>
2020-04-21 11:53:52 +0900

    doneの削除とcomplete_atの追加

--- todo/models.py
+++ todo/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class User(models.Model):
     username = models.CharField("ユーザー名", max_length=50)
-    email = models.EmailField("パスワード", max_length=255)
-    password = models.CharField("メールアドレス", max_length=50)
+    email = models.EmailField("メールアドレス", max_length=255)
+    password = models.CharField("パスワード", max_length=50)
 
     def __str__(self):
         return self.username
@@ -17,7 +17,7 @@ class User(models.Model):
 class Todo(models.Model):
     title = models.CharField("トレーニング名", max_length=50)
     times = models.PositiveIntegerField("回数")
-    done = models.BooleanField("status", default=False)
+    complete_at = models.DateTimeField('完了日時', null=True)
     user = models.ForeignKey("User", on_delete=models.CASCADE)
 
     def __str__(self):
@@ -27,4 +27,3 @@ class Todo(models.Model):
         db_table = "todo"
         verbose_name = "TODO"
         verbose_name_plural = "TODOs"
-        
