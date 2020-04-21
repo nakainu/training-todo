42e471636488e80e966d0e822e8a640f49c07f72
Masaya Nakayama <uw.msy.0913@gmail.com>
2020-04-21 11:26:37 +0900

    create models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField("ユーザー名", max_length=50)
+    email = models.EmailField("パスワード", max_length=255)
+    password = models.CharField("メールアドレス", max_length=50)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        db_table = "user"
+        verbose_name = "ユーザー"
+        verbose_name_plural = "ユーザー"
+
+
+class Todo(models.Model):
+    title = models.CharField("トレーニング名", max_length=50)
+    times = models.PositiveIntegerField("回数")
+    done = models.BooleanField("status", default=False)
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = "todo"
+        verbose_name = "TODO"
+        verbose_name_plural = "TODOs"
+        
