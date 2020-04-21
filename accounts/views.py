d0cc02b63497aa50161a5dac12165e923c86f599
Masaya Nakayama <uw.msy.0913@gmail.com>
2020-04-22 01:10:24 +0900

    signupの実装を簡略化

--- accounts/views.py
+++ accounts/views.py
@@ -11,29 +11,12 @@ from django.contrib.auth.forms import UserCreationForm
 class Index(TemplateView):
     template_name = 'index.html'
 
-index = Index.as_view()
-
 """
 アカウントの作成
 UserCreationFormクラスで新しいユーザを作成する
-password1 と password2 が一致するか確認し、検証
 """
-class CreateAccount(CreateView):
-    def post(self, request, *args, **kwargs):
-        form = UserCreationForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            #フォームから'username'を読み取る
-            username = form.cleaned_data.get('username')
-            #フォームから'password'を読み取る
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            return redirect('/')
-        return render(request, 'create.html', {'form': form,})
-
-    def get(self, request, *args, **kwargs):
-        form = UserCreationForm(request.POST)
-        return  render(request, 'create.html', {'form': form,})
-
-create_account = CreateAccount.as_view()
+class SignUp(CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = "signup.html"
+    success_url = "/"
