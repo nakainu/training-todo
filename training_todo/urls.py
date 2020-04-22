694e18268b2419f51ddc0dd3aaa24048d3d8723c
Masaya Nakayama <uw.msy.0913@gmail.com>
2020-04-22 14:14:38 +0900

    ログインログアウト機能の実装

--- training_todo/urls.py
+++ training_todo/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from accounts import views
 
 
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
     path('signup/', views.SignUp.as_view(), name='signup'),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
