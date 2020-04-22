5fad015a1f914bd6285ce91872e2f8ff1507542b
Masaya Nakayama <uw.msy.0913@gmail.com>
2020-04-22 14:13:15 +0900

    使わないimportの削除

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
 from django.views.generic import CreateView, TemplateView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 
 
 class Index(TemplateView):
