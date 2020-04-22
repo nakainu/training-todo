3d0e3903fd30bbe13d53b305f42b3f8f3545af99
Masaya Nakayama <uw.msy.0913@gmail.com>
2020-04-23 08:11:53 +0900

    idの追加

--- todo/admin.py
+++ todo/admin.py
@@ -4,6 +4,6 @@ from todo.models import Todo
 
 class TodoAdmin(admin.ModelAdmin):
     # 一覧表示で表示するフィールドを設定
-    list_display = ('title', 'times', 'complete_at', 'user')
+    list_display = ('id', 'title', 'times', 'complete_at', 'user')
 
 admin.site.register(Todo, TodoAdmin)
