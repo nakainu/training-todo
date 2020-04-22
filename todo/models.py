b5a3bcbd53313724e2b966abf12cdb6c7ba7fbcf
Masaya Nakayama <uw.msy.0913@gmail.com>
2020-04-22 15:46:25 +0900

    modelsの追加、migrateファイルの追加

--- todo/models.py
+++ todo/models.py
@@ -1,9 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 class Todo(models.Model):
     title = models.CharField("トレーニング名", max_length=50)
     times = models.PositiveIntegerField("回数")
     complete_at = models.DateTimeField('完了日時', null=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
