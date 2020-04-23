7e806e4d6804bc3ae6ba313ef69de8ba5027285a
Masaya Nakayama <uw.msy.0913@gmail.com>
2020-04-23 13:46:47 +0900

    todo表示の実装

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from todo.models import Todo
 from django.contrib.auth.models import User
 from todo.forms import TodoSettingForm
+
+
+def todo_list(request):
+    """
+    userのtodoの全てのオブジェクトを取得して並べる．
+    """
+    todos = Todo.objects.filter(user_id = request.user.id).order_by()
+    return render(request, 'todo.html', {'todos' : todos})
+
 
 def add_todo(request):
     """
@@ -17,6 +26,7 @@ def add_todo(request):
                 times = form.cleaned_data['times'],
                 user_id = request.user.id
             )
+        return redirect('todo')
     else:   
         form = TodoSettingForm()
     return render(request, 'todo_setting.html', {'form': form})
