485bc5874e3ac40a049b57b24d3178f85f8cc5ff
Masaya Nakayama <uw.msy.0913@gmail.com>
2020-04-23 10:39:25 +0900

    設定画面の実装

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from todo.models import Todo
+from django.contrib.auth.models import User
+from todo.forms import TodoSettingForm
 
-# Create your views here.
+def add_todo(request):
+    """
+    formの情報をtodoテーブルに格納．
+    ログインしているuser_idと紐付ける．
+    """
+    form = TodoSettingForm(request.POST or None)
+    # fieldの値が正しいかを検証
+    if form.is_valid():
+        todo = Todo.objects.create(
+            title = form.cleaned_data['title'],
+            times = form.cleaned_data['times'],
+            user_id = request.user.id
+        )
+    return render(request, 'todo_setting.html', {'form': form})
