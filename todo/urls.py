df90ca6b8ce0c40d9097e7b6dab900898c1a280b
Masaya Nakayama <uw.msy.0913@gmail.com>
2020-04-25 18:11:34 +0900

    削除機能の実装

--- todo/urls.py
+++ todo/urls.py
@@ -5,5 +5,6 @@ from todo import views
 urlpatterns = [
     path('', views.todo_list, name='todo'),
     path('complete/<int:todo_id>/', views.complete, name='complete'),
+    path('delete/<int:todo_id>/', views.delete, name='delete'),
     path('setting', views.add_todo, name='todo_setting'),
 ]
